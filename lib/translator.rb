394e818835d200de7544c744d5ca03d860430a8c
Cameron Bisca <cjbisca@gmail.com>
2020-11-10 18:06:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@
 require 'yaml' 
 
 def load_library(path)
-  emoticons = {"get_meaning" => {}, "get_emoticon" => {}}
+  emoticons = {"emotion" => {}, "get_emoticon" => {}}
   
   YAML.load_file(path).each do |meaning, describe|
      eng, jan = describe
