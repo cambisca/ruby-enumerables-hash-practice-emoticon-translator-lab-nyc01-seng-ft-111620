068f4c2f304381e1a223e0d06cf3610f94bf57d0
Cameron Bisca <cjbisca@gmail.com>
2020-11-10 18:12:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -30,14 +30,13 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   emoticons = load_library(path) #call load_library
-  result = emoticons["equiv_emoticon"][emoticon]
+  result = emoticons["get_emoticon"][emoticon]
   if result
     result
   else
     "Sorry, that emoticon was not found"
   end
 end
-
 
 def get_english_meaning(path, emoticon)
   emoticons = load_library(path)
