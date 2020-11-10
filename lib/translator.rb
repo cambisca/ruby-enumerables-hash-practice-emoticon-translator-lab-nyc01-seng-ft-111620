8ea4fb3dbc9940ed478e82f21da2742477683f6a
Cameron Bisca <cjbisca@gmail.com>
2020-11-10 18:00:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,32 @@
-# require modules here
+# {
+#   "angel" => [ "O:)", "☜(⌒▽⌒)☞" ],
+#   "angry" => [ ">:(", "ヽ(ｏ`皿′ｏ)ﾉ" ],
+#   "bored" => [ ":O", "(ΘεΘ;)" ],
+#   "confused" => [ "%)", "(゜.゜)" ],
+#   "embarrassed" => [ ":$", "(#^.^#)" ],
+#   "fish" => [ "><>", ">゜))))彡" ],
+#   "glasses" => [ "8D", "(^0_0^)" ],
+#   "grinning" => [ "=D", "(￣ー￣)" ],
+#   "happy" => [ ":)", "(＾ｖ＾)" ],
+#   "kiss" => [ ":*", "(*^3^)/~☆" ],
+#   "sad" => [ ":'(", "(Ｔ▽Ｔ)" ],
+#   "surprised" => [ ":o", "o_O" ],
+#   "wink" => [ ";)", "(^_-)" ]
+# }
 
-def load_library
-  # code goes here
+
+def load_library(path)
+  require 'yaml' 
+  emojis = YAML.load_file('lib/emoticons.yml')
+  emojis.map do |emotion|
+    
+
 end
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
 def get_english_meaning
-  # code goes here
+
 end
